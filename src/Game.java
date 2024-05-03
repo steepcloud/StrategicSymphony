9edb977f1df76da4d1d5a2e68f561c6d9d2502d1
David Carboveanu <130596873+steepcloud@users.noreply.github.com>
2024-05-03 19:46:42 +0300

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,22 @@
 import java.util.List;
 import java.util.Random;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Game {
-	private int[][] board;
-	private int numRows = 6;
-	private int numCols = 10;
+	public int[][] board;
+	public int numRows = 6;
+	public int numCols = 10;
 	private List<ClientHandler> players;
+	public int currentTurn;
+	private boolean keepTurn = false;
+	private Server server;
 	
-	public Game(int numRows, int numCols, List<ClientHandler> players) {
+	public Game(int numRows, int numCols, List<ClientHandler> players, Server server) {
 		this.numRows = numRows;
 		this.numCols = numCols;
 		this.board = new int[numRows][numCols];
 		this.players = players;
+		currentTurn = 0;
+		this.server = server;
 	}
 	
 	public void startGame() {
@@ -34,5 +36,27 @@ public class Game {
         	}
         	System.out.println();
         }
+	}
+	
+	public Boolean putPiece(int x, int y, int id) {
+		if (id == players.get(currentTurn).id) {
+			board[x][y] = id;
+			advanceTurn(); 
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	private void advanceTurn() {
+		if (!keepTurn) {
+			currentTurn = (currentTurn + 1) % players.size();
+		}
+		else {
+			keepTurn = false;
+		}
+		
+		server.updatePlayers();
 	}
 }
