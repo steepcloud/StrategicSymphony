3d3179820cfafe2371f28fbc6f71a785255c964e
David Carboveanu <130596873+steepcloud@users.noreply.github.com>
2024-05-04 18:32:01 +0300

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -7,8 +7,10 @@ public class Game {
 	private int numCols = 10;
 	private List<ClientHandler> players;
 	public int currentTurn;
-	private boolean keepTurn = false;
 	private Server server;
+	private boolean doubleMoveJokerUsed = false;
+    private Boolean replaceJokerUsed = false;
+    private Boolean freedomJokerUsed = false;
 	
 	public Game(int numRows, int numCols, List<ClientHandler> players, Server server) {
 		this.numRows = numRows;
@@ -31,6 +33,34 @@ public class Game {
 		return board[x][y];
 	}
 	
+	public Boolean getFreedomJoker() {
+		return freedomJokerUsed;
+	}
+	
+	public Boolean getDoubleMoveJoker() {
+		return doubleMoveJokerUsed;
+	}
+	
+	public Boolean getReplaceJoker() {
+		return replaceJokerUsed;
+	}
+	
+	public void setFreedomJoker(Boolean freedomJokerUsed) {
+		this.freedomJokerUsed = freedomJokerUsed;
+	}
+	
+	public void setReplaceJoker(Boolean replaceJokerUsed) {
+		this.replaceJokerUsed = replaceJokerUsed;
+	}
+	
+	public void setDoubleMoveJoker(Boolean doubleMoveJokerUsed) {
+		this.doubleMoveJokerUsed = doubleMoveJokerUsed;
+	}
+	
+	public Boolean checkTurn(int id) {
+		return id == players.get(currentTurn).getId();
+	}
+	
 	public void startGame() {
 		Random rand = new Random();
         for (ClientHandler player : players) {
@@ -51,9 +81,10 @@ public class Game {
 	}
 	
 	public Boolean putPiece(int x, int y, int id) {
-		if (id == players.get(currentTurn).getId()) {
+		if (checkTurn(id)) {
 			board[x][y] = id;
-			advanceTurn(); 
+			advanceTurn();
+			resetJokers();
 			return true;
 		}
 		else {
@@ -62,13 +93,49 @@ public class Game {
 	}
 	
 	private void advanceTurn() {
-		if (!keepTurn) {
+		if (!doubleMoveJokerUsed) {
 			currentTurn = (currentTurn + 1) % players.size();
 		}
 		else {
-			keepTurn = false;
+			doubleMoveJokerUsed = false;
 		}
 		
 		server.updatePlayers();
+	}
+	
+	public Boolean isValidMove(int row, int col, int colorId) {
+		// check cell within bounds
+		if (row < 0 || row >= numRows || col < 0 || col >= numCols) {
+			return false;
+		}
+		
+		// check for empty cell
+		if (!replaceJokerUsed && board[row][col] != 0 || board[row][col] == colorId) {
+			return false;
+		}
+		
+		// check neighboring cells
+		if (!freedomJokerUsed) {
+			for (int i = row - 1; i <= row + 1; i++) {
+				for (int j = col - 1; j <= col + 1; j++) {
+					if (i >= 0 && i < numRows && j >= 0 && j < numCols) {
+						if (board[i][j] == colorId) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		else {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void resetJokers() {
+		doubleMoveJokerUsed = false;
+		replaceJokerUsed = false;
+		freedomJokerUsed = false;
 	}
 }
