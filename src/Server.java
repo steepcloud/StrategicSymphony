e3f9e9b1be67e7e292056eb43c4690a2b4e2a153
David Carboveanu <130596873+steepcloud@users.noreply.github.com>
2024-05-08 23:16:48 +0300

    add max clients and others

--- src/Server.java
+++ src/Server.java
@@ -9,24 +9,56 @@ public class Server {
     private List<ClientHandler> clients = new CopyOnWriteArrayList<>();
     private Game game;
     private int nextClientId = 1;
+    private static final int MAX_CLIENTS = 5;
 
     public Server() throws IOException {
         serverSocket = new ServerSocket(PORT);
         System.out.println("Server started on port " + PORT);
     }
-
+    
     public void startServer() {
         try {
             while (true) {
-                Socket socket = serverSocket.accept();
-                ClientHandler clientHandler = new ClientHandler(socket, this, nextClientId);
-                nextClientId++;
-                clients.add(clientHandler);
-                new Thread(clientHandler).start();
+            	if (clients.size() < MAX_CLIENTS) {
+	                Socket socket = serverSocket.accept();
+	                ClientHandler clientHandler = new ClientHandler(socket, this, nextClientId);
+	                nextClientId++;
+	                clients.add(clientHandler);
+	                new Thread(clientHandler).start();
+            	} else {
+            		System.out.println("Limita maxima de clienti a fost atinsa.");
+            		break;
+            	}
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    public void stopServer() {
+        try {
+            if (serverSocket != null && !serverSocket.isClosed()) {
+                serverSocket.close();
+            }
+            
+            for (ClientHandler client : clients) {
+                client.stop();
+            }
+        } catch (IOException ex) {
+            System.err.println("Error stopping the server: " + ex.getMessage());
+        }
+    }
+    
+    public int getNumClients() {
+        return clients.size();
+    }
+    
+    public void addClient(ClientHandler client) {
+        clients.add(client);
+    }
+    
+    public List<ClientHandler> getClients() {
+        return clients;
     }
 
     public void removeClient(ClientHandler client) {
@@ -49,33 +81,69 @@ public class Server {
     }
     
     public void putPiece(int x, int y, ClientHandler client) {
-    	if (game.putPiece(x, y, client.getId())) {
-    		client.sendMessage("Piece was placed.");
-    		System.out.println("Piece was placed.");
+    	if (game.isValidMove(x, y, client.getId())) {
+	    	if (game.putPiece(x, y, client.getId())) {
+	    		client.sendMessage("Piece was placed.");
+	    		System.out.println("Piece was placed.");
+	    	}
+	    	else {
+	    		client.sendMessage("Not your turn");
+	    		System.out.println("Not your turn.");
+	    	}
+    	} else {
+    		client.sendMessage("Invalid move. You can only place a piece in an empty cell with at least one neighboring cell of the same color.");
+            System.out.println("Invalid move. Player " + client.getUserName() + " tried to place piece at (" + x + ", " + y + ").");
     	}
-    	else {
-    		client.sendMessage("Not your turn");
-    		System.out.println("Not your turn.");
+    }
+    
+    public void toggleFreedomJoker(ClientHandler client) {
+    	if (game.checkTurn(client.getId())) {
+    		game.setFreedomJoker(!game.getFreedomJoker());
+    		client.sendMessage("Freedom joker is now " + (game.getFreedomJoker() ? "on." : "off."));
+    	} else {
+    		client.sendMessage("Not your turn.");
+    	}
+    }
+    
+    public void toggleReplaceJoker(ClientHandler client) {
+    	if (game.checkTurn(client.getId())) {
+    		game.setReplaceJoker(!game.getReplaceJoker());
+    		client.sendMessage("Replace joker is now " + (game.getReplaceJoker() ? "on." : "off."));
+    	} else {
+    		client.sendMessage("Not your turn.");
+    	}
+    }
+    
+    public void toggleDoubleMoveJoker(ClientHandler client) {
+    	if (game.checkTurn(client.getId())) {
+    		game.setDoubleMoveJoker(!game.getDoubleMoveJoker());
+    		client.sendMessage("Double move joker is now " + (game.getDoubleMoveJoker() ? "on." : "off."));
+    	} else {
+    		client.sendMessage("Not your turn.");
     	}
     }
     
     public void changeColor(ClientHandler client, String color) {
-    	//check if color exists
-    	if (client.getColors().contains(color)) {
-    		
-        	if (isColorAvailable(color)) {
-            	client.setColor(color);
-            	client.sendMessage("Color changed to " + color);
-        		System.out.println(client.getUserName() + " changed to " + color);
-        		
-        		sendReadyStatusToOtherPlayers(client);
-        	}
-        	else {
-        		client.sendMessage("Color is already taken");
-        	}
-    	}
-    	else {
-    		client.sendMessage(color + " is not a valid color");
+    	if (client.isGameStarted()) {
+    		client.sendMessage("Game has already started. You cannot change color.");
+    	} else {
+	    	//check if color exists
+	    	if (client.getColors().contains(color)) {
+	    		
+	        	if (isColorAvailable(color)) {
+	            	client.setColor(color);
+	            	client.sendMessage("Color changed to " + color);
+	        		System.out.println(client.getUserName() + " changed to " + color);
+	        		
+	        		sendReadyStatusToOtherPlayers(client);
+	        	}
+	        	else {
+	        		client.sendMessage("Color is already taken");
+	        	}
+	    	}
+	    	else {
+	    		client.sendMessage(color + " is not a valid color");
+	    	}
     	}
     }
     
@@ -92,9 +160,18 @@ public class Server {
     }
     
     public Boolean isUniqueUserName(String name) {
+    	if (name == null) {
+    		return false;
+    	}
+    	
+        int count = 0;
         for (ClientHandler client : clients) {
-            if (client.getUserName().trim().equalsIgnoreCase(name.trim())) {
-                return false;
+        	System.out.println(client.getUserName());
+            if (client.getUserName().equalsIgnoreCase(name)) {
+                count++;
+                if (count > 1) {
+                    return false;
+                }
             }
         }
         return true;
@@ -133,6 +210,9 @@ public class Server {
     		}
     	}
     	System.out.println("Game ready to start");
+    	for (ClientHandler client : clients) {
+    		client.setGameStarted(true);
+    	}
     	game = new Game(6, 10, clients, this);
     	game.startGame();
     	updatePlayers();
@@ -152,8 +232,41 @@ public class Server {
     	}
     }
     
+    public ClientHandler getHandlerById(int id) throws Exception {
+    	for (ClientHandler client : this.clients) {
+    		if (client.getId() == id) {
+    			return client;
+    		}
+    	}
+    	throw new Exception("No Handler found for id:"+id);
+    }
+    
+    public void pass(ClientHandler client) {
+    	if (game.checkTurn(client.getId())) {
+    		game.pass(client);
+    		if(!game.checkEnd()) {
+        		updatePlayers();
+    		}else {
+    			endGame();
+    		}
+    	}
+    	else {
+    		client.sendMessage("Not your turn");
+    	}
+    }
+    
+    private void endGame() {
+    	ClientHandler winner = game.findWinner();
+
+    	for (int i = 0; i < clients.size(); i++) {
+    		System.out.println(clients.get(i).getUserName());
+    		clients.get(i).sendMessage("The winner is "+winner.getUserName());
+    	}
+    }
+    
     public static void main(String[] args) throws IOException {
         Server server = new Server();
         server.startServer();
     }
+    
 }
